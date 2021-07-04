695d50c22bfb707814a34c0e7641614905be5d01
VADuarte <vividua@gmail.com>
2021-07-04 17:57:26 -0300

    Project read files.

--- src/main/java/files/test/files/rest/api/controller/ReadFileController.java
+++ src/main/java/files/test/files/rest/api/controller/ReadFileController.java
@@ -1,40 +1,12 @@
 package files.test.files.rest.api.controller;
 
-import ch.qos.logback.core.rolling.helper.FileNamePattern;
-import files.test.files.rest.api.model.FileReadedInformation;
 import files.test.files.rest.api.model.FilesInformationDto;
-import files.test.files.rest.api.service.FileReadService;
 import files.test.files.rest.api.service.FileReadServiceImpl;
-
-import files.test.files.rest.api.thread.MainThread;
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
-
-import javax.annotation.PostConstruct;
-import javax.net.ssl.HttpsURLConnection;
-import javax.swing.filechooser.FileNameExtensionFilter;
-import java.io.*;
-import java.net.*;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
-import java.nio.file.*;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
-//Rest Application
+
 @RestController
 
 @RequestMapping("/api/read")
@@ -44,10 +16,7 @@ public class ReadFileController{
     private Map<String,FilesInformationDto> mapFiles=null;
     private List<FilesInformationDto> listDto = null;
 
-    //private MainThread mainThread;
-
-
-     FileReadServiceImpl service;
+    FileReadServiceImpl service;
 
    @RequestMapping(value="/",method=RequestMethod.GET)
     public String check() {
@@ -66,12 +35,7 @@ public class ReadFileController{
             }
             return strReturn;
          }catch(Exception e){
-           e.printStackTrace();
-
            return e.getMessage();
        }
-    //return "ok";
    }
-
-
-    }
+  }
